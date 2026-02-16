8f942bfbcccb83f2a908d32f80e775bb54daa908
Abhinav Kumar <abhinavku04@gmail.com>
2026-02-16 18:51:55 +0530

    Update login API URL to use environment variable

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -25,7 +25,7 @@ const Login = () => {
         setLoading(true);
 
         try {
-            const res = await axios.post("http://localhost:5000/manora/auth/login", form);
+            const res = await axios.post(`${import.meta.env.VITE_API_URL}/manora/auth/login`, form);
             login(res.data.token, res.data.user);
             navigate("/");
         } catch (err) {
