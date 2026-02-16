760671f2ff322bc228c34f4cc2ae99e428063c39
Abhinav Kumar <abhinavku04@gmail.com>
2026-02-16 18:41:52 +0530

    Update API_URL to use environment variable

--- src/services/galleryService.js
+++ src/services/galleryService.js
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/manora/gallery';
+const API_URL = `${import.meta.env.VITE_API_URL}/manora/gallery`;
 
 export const uploadPhoto = async (formData, token) => {
     const config = {
